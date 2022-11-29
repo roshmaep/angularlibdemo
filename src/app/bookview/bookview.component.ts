582c326a0be86add1758378a8e92ca98d8e18165
roshmaep <roshmaep1999@gmail.com>
2022-11-29 22:00:27 +0530

    book view

---
+++ src/app/bookview/bookview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookview',
+  templateUrl: './bookview.component.html',
+  styleUrls: ['./bookview.component.css']
+})
+export class BookviewComponent {
+
+}
