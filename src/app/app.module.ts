c4138e454882ed4d2d75428461aa706222084d1e
roshmaep <roshmaep1999@gmail.com>
2022-11-29 20:49:56 +0530

    bookentery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookentryComponent } from './bookentry/bookentry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookentryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bookentry/bookentry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookentry',
+  templateUrl: './bookentry.component.html',
+  styleUrls: ['./bookentry.component.css']
+})
+export class BookentryComponent {
+
+}
